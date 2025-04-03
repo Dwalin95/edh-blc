34654f8166cb75756e8277223de561081c6f2444
Andrea Cheloni <andrea.cheloni@emeal.nttdata.com>
2025-04-03 13:00:10 +0200

    aggiunto caricamento loading per le liste

--- src/pages/SearchList.tsx
+++ src/pages/SearchList.tsx
@@ -23,7 +23,18 @@ const SearchList: React.FC = () => {
             height={10}
             smoothness={true}
           />
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={<div className="flex flex-col items-center py-10">
+            <iframe
+              src="https://giphy.com/embed/13hzdQ3QCID172"
+              className="w-full max-w-xs sm:max-w-md h-auto"
+              frameBorder="0"
+              allowFullScreen
+            ></iframe>
+            <p className="italic text-gray-600 dark:text-gray-400 mt-4">
+              Sto Caricando le liste!
+            </p>
+            
+          </div>}>
             <LazySiteBarLeft />
             <LazyHeaderIntroRicerca />
             <LazySiteBarRight />
